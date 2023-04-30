311dbbce512eb57d9a3c9cd26c572239ecb7b1cb
2732ii2 <mo.anas123khan@gmail.com>
2023-04-30 15:35:21 +0530

    craziet_design

--- arsolv/src/Left_part.js
+++ arsolv/src/Left_part.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import Img from "./d1.jpg";
+import Img1 from "./d3.jpg";
+
+import Img2 from "./d2.jpg";
+import Img3 from "./d1.jpg";
+
+
+import Img4 from "./d4.jpg";
+
+import Img5 from "./d5.jpg";
+
 export default function Left_part() {
   return (
     <div className="Left_Side">
       <div id="l_up">
-        <img src={Img} style={{ width: "100%", height: "100%" }} />
+        <img src={Img3} style={{ width: "100%", height: "100%" }} />
       </div>
       <div id="l_down">
         <div id="first">
@@ -14,11 +23,21 @@ export default function Left_part() {
           <ArrowLeftIcon style={{ width: "100px", height: "100px" }} />{" "}
         </div>
         <div id="second">
-          <div id="one"></div>
-          <div id="one"></div>
-          <div id="one"></div>
-          <div id="one"></div>
-          <div id="one"></div>
+          <div id="one">
+            <img src={Img1} style={{ width: "100%", height: "100%" }} />
+          </div>
+          <div id="one">
+            <img src={Img2} style={{ width: "100%", height: "100%" }} />
+          </div>
+          <div className="so" id="one">
+            <img src={Img3} style={{ width: "100%", height: "100%" }} />
+          </div>
+          <div id="one">
+            <img src={Img4} style={{ width: "100%", height: "100%" }} />
+          </div>
+          <div id="one">
+            <img src={Img5} style={{ width: "100%", height: "100%" }} />
+          </div>
         </div>
         <div id="third">
           {" "}
